a3d6744aed590c68a863f51d7150ce2d09db096e
msho <mmmmsho@gmail.com>
2019-05-22 00:06:48 +0300

    refactoring

--- dom/main.js
+++ dom/main.js
@@ -8,29 +8,43 @@ window.addEventListener('hashchange', locationHasChanged); //url hash has change
 function locationHasChanged() {
     window.taskDetailTries = 0;
 
-    if (location.hash && location.hash.indexOf('taskdetail/') > 0) {
-        chrome.runtime.sendMessage({ type: 'action-icon', isTaskDetail: false });
-        whenTaskDetailReady().then(isReady => {
-            console.log(`is task-detail ready: ${isReady}`);
-            chrome.runtime.sendMessage({ type: 'action-icon', isTaskDetail: isReady });
+    // I'm not sure im at task page yet
+    onNotTaskPage();
 
-            let domInit = Scraper.getDomData();
-            console.log(domInit);
-        } //task ready
-        ).catch(exp => {console.log(exp)});
+    if (location.hash && location.hash.indexOf('taskdetail/') > 0) {
+
+        waitTaskDetailReady().then(isReady => {
+            if (isReady) 
+                onTaskReady();
+            else 
+                onNotTaskPage();
+        }); //task ready
+        
     } else {
-        console.log(`is task-detail ready: false`);
-        chrome.runtime.sendMessage({ isTaskDetail: false });
+        onNotTaskPage();
     }
 
 } //locationHasChanged
 
-function whenTaskDetailReady() {
+function onTaskReady() {
+    console.log(`is task-detail ready: true`);
+    chrome.runtime.sendMessage({ type: 'action-icon', isTaskDetail: true });
+
+    let domInit = Scraper.getDomData();
+    console.log(domInit);
+    chrome.runtime.sendMessage({ isTaskDetail: false });
+}
+
+function onNotTaskPage() {
+    console.log(`is task-detail ready: false`);
+    chrome.runtime.sendMessage({ type: 'action-icon', isTaskDetail: false });
+}
+
+function waitTaskDetailReady() {
     return new Promise(resolve => {
 
         clearInterval(window.isReadyInterval);
         window.isReadyInterval = setInterval(() => {
-            console.log(`waiting for task to be ready ${window.taskDetailTries} times`);
 
             if (Scraper.isLabelFieldExist('Owner') && Scraper.isElementIdExist('username')) {
                 // inside task details
@@ -44,14 +58,9 @@ function whenTaskDetailReady() {
                 return resolve(false);
             }
 
-        }, 300);
-        
-        /*if (Scraper.isLabelFieldExist('Owner') && Scraper.isElementIdExist('username')) {
-            // inside task details
-            resolve(true);
-        } else {
-            setTimeout(whenTaskDetailReady, 800);
-        }*/
+            console.log(`waiting for task to be ready ${window.taskDetailTries} times`);
+
+        }, 200); // setInterval
 
     }); //Promise
 } //whenTaskDetailReady
